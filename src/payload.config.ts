36294a18a0bdf57df5195095fc71ba944a4959cb
neilgardner1963@gmail.com <neilgardner1963@gmail.com>
2023-11-02 14:56:09 +0000

    initial commit

---
+++ src/payload.config.ts
@@ -0,0 +1,26 @@
+import { buildConfig } from 'payload/config';
+import path from 'path';
+// import Examples from './collections/Examples';
+import Users from './collections/Users';
+import { slateEditor } from '@payloadcms/richtext-slate';
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+
+export default buildConfig({
+  serverURL: 'http://localhost:3000',
+  admin: {
+    user: Users.slug,
+  },
+  collections: [
+    Users,
+    // Add Collections here
+    // Examples,
+  ],
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+  },
+  db: mongooseAdapter({url: process.env.MONGODB_URI }),
+  editor: slateEditor({})
+})
